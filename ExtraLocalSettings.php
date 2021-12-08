f443cde92876bf122dc9bf7f05b79bbca8244843
soup-bowl <code@soupbowl.io>
2021-12-08 16:06:47 +0000

    Extra settings page.

---
+++ ExtraLocalSettings.php
@@ -0,0 +1,7 @@
+<?php
+# Add extra SSO settings here.
+# $wgOAuthCustomAuthProviders = [ 'WordPress' => AuthenticationProvider\WordPressAuth::class ];
+# $wgOAuthAuthProvider        = 'WordPress';
+# $wgOAuthUri                 = 'http://localhost:8080/wp-admin';
+# $wgOAuthClientId            = 'abc123zyx987abc123zyx987';
+# $wgOAuthClientSecret        = 'abc123zyx987abc123zyx987';
